d0b016d1f1393aeb039e620584af4b23ad6a8ae5
yummongi <101652932+yummongi@users.noreply.github.com>
2024-10-12 13:53:54 +0900

    [섹션5-6] 검증1, 검증2 - Bean Validation 완료
    - 검증 요구사항 분석 및 직접 처리 방식 학습
    - BindingResult, FieldError, ObjectError 개념 학습
    - 오류 코드와 메시지 처리 방법 실습 및 Validator 분리 적용
    - Bean Validation을 활용한 검증 자동화 및 한계 이해
    - groups 개념 학습 및 Form 전송 객체 분리 실습
    - HTTP 메시지 컨버터를 통한 Validation 적용 완료

---
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,36 @@
+package hello.itemservice.domain.item;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+@Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000") //권장하지 않음
+public class Item {
+
+    //@NotNull(groups = UpdateCheck.class)
+    private Long id;
+
+    //@NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    private String itemName;
+
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+    private Integer price;
+
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Max(value = 9999, groups = {SaveCheck.class})
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
---
+++ validation/src/test/java/hello/itemservice/validation/messageCodesResolverTest.java
@@ -0,0 +1,39 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class messageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+
+
+    }
+
+}
